a4c119ddea37c4c1b79100dd211d909abe2ecc74
divya.loganathan <thilodivya@gmail.com>
2022-11-29 17:29:04 +0530

    latest change for progressive profiling

---
+++ pages/profileUpdate.jsx
@@ -0,0 +1,22 @@
+import { redirect } from 'next/dist/server/api-utils';
+import React from 'react';
+
+
+export default function PageWithoutJSbasedForm() {
+    const handleSubmit = async (event) => {
+    
+        
+    }
+    return (
+        <form action="http://localhost:3000/profile" method="POST">
+        <center><label htmlFor="company_name">Company Name</label></center>
+        <center><input type="text" id="company_name" name="company_name" required /></center>
+        <br></br>
+        <center><label htmlFor="job_title">Job Title</label></center>
+        <center><input type="text" id="job_title" name="job_title" required /></center>
+        <br></br>
+        <br></br>
+        <center><button type="submit">Submit</button></center>
+      </form>
+    )
+  }
