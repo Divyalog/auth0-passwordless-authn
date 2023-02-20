cbbbd009e153a1da6543b353649a519789d7bc79
divya.loganathan <thilodivya@gmail.com>
2023-02-20 14:25:01 +0530

    latest progressive profiling changes

--- pages/newtest.jsx
+++ pages/newtest.jsx
@@ -1,13 +1,17 @@
 var axios = require("axios").default;
 import { useRouter } from "next/router";
+const jwt = require('jsonwebtoken');
 
-export default function Items(props) {
-  var router = useRouter();
-  var id = router.query["company_name"];
-  var id2 = router.query["job_title"];
-  const response = updateUser(id, id2);
+export default function updateUserProfile(props) {
+  console.log("Entering updateUserProfile method");
+  const router = useRouter();
+  const id = router.query["company_name"];
+  const id2 = router.query["job_title"]; 
+  const id3 = props.session_token;
+  updateUser(id, id2, id3);
+  console.log("Exiting updateUserProfile method");
 return (
-  <form action='http://localhost:3000/api/auth/login' method='POST'>
+  <form>
           <center><h1>Welcome to Auth0</h1></center><br></br><br></br>
           <h4>Below are the values that you have updated</h4>
           <p><b>Company Name:</b> {id}</p>
@@ -17,11 +21,20 @@ return (
       )
 }
 
-function updateUser(id, id2) {
+function updateUser(id, id2, session) {
+  let userDetail;
+  var userId;
+  try{
+  userDetail = jwt.verify(session, process.env.AUTH0_SECRET);
+  userId = userDetail.sub;
+  console.log(`Jwt token verification is success for this user: ${userId}`);
+} catch (e){
+  console.log('Error in verifying jwt token');
+}
 var options = {
   method: 'PATCH',
-  url: 'https://testtenantpasswordlessauthentication.us.auth0.com/api/v2/users/auth0|637bb1d9149ff97d30762306',
-  headers: {authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5pSGI0YTBtUEUydDM2ODRVY3JGRSJ9.eyJpc3MiOiJodHRwczovL3Rlc3R0ZW5hbnRwYXNzd29yZGxlc3NhdXRoZW50aWNhdGlvbi51cy5hdXRoMC5jb20vIiwic3ViIjoiQzBlTnBHTndsb2RrN3ZVN0xUcXBtSnc3bGNYNnExaGRAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vdGVzdHRlbmFudHBhc3N3b3JkbGVzc2F1dGhlbnRpY2F0aW9uLnVzLmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNjc1OTUxNzE1LCJleHAiOjE2NzY4MTU3MjYsImF6cCI6IkMwZU5wR053bG9kazd2VTdMVHFwbUp3N2xjWDZxMWhkIiwic2NvcGUiOiJyZWFkOmNsaWVudF9ncmFudHMgY3JlYXRlOmNsaWVudF9ncmFudHMgZGVsZXRlOmNsaWVudF9ncmFudHMgdXBkYXRlOmNsaWVudF9ncmFudHMgcmVhZDp1c2VycyB1cGRhdGU6dXNlcnMgZGVsZXRlOnVzZXJzIGNyZWF0ZTp1c2VycyByZWFkOnVzZXJzX2FwcF9tZXRhZGF0YSB1cGRhdGU6dXNlcnNfYXBwX21ldGFkYXRhIGRlbGV0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgY3JlYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSByZWFkOnVzZXJfY3VzdG9tX2Jsb2NrcyBjcmVhdGU6dXNlcl9jdXN0b21fYmxvY2tzIGRlbGV0ZTp1c2VyX2N1c3RvbV9ibG9ja3MgY3JlYXRlOnVzZXJfdGlja2V0cyByZWFkOmNsaWVudHMgdXBkYXRlOmNsaWVudHMgZGVsZXRlOmNsaWVudHMgY3JlYXRlOmNsaWVudHMgcmVhZDpjbGllbnRfa2V5cyB1cGRhdGU6Y2xpZW50X2tleXMgZGVsZXRlOmNsaWVudF9rZXlzIGNyZWF0ZTpjbGllbnRfa2V5cyByZWFkOmNvbm5lY3Rpb25zIHVwZGF0ZTpjb25uZWN0aW9ucyBkZWxldGU6Y29ubmVjdGlvbnMgY3JlYXRlOmNvbm5lY3Rpb25zIHJlYWQ6cmVzb3VyY2Vfc2VydmVycyB1cGRhdGU6cmVzb3VyY2Vfc2VydmVycyBkZWxldGU6cmVzb3VyY2Vfc2VydmVycyBjcmVhdGU6cmVzb3VyY2Vfc2VydmVycyByZWFkOmRldmljZV9jcmVkZW50aWFscyB1cGRhdGU6ZGV2aWNlX2NyZWRlbnRpYWxzIGRlbGV0ZTpkZXZpY2VfY3JlZGVudGlhbHMgY3JlYXRlOmRldmljZV9jcmVkZW50aWFscyByZWFkOnJ1bGVzIHVwZGF0ZTpydWxlcyBkZWxldGU6cnVsZXMgY3JlYXRlOnJ1bGVzIHJlYWQ6cnVsZXNfY29uZmlncyB1cGRhdGU6cnVsZXNfY29uZmlncyBkZWxldGU6cnVsZXNfY29uZmlncyByZWFkOmhvb2tzIHVwZGF0ZTpob29rcyBkZWxldGU6aG9va3MgY3JlYXRlOmhvb2tzIHJlYWQ6YWN0aW9ucyB1cGRhdGU6YWN0aW9ucyBkZWxldGU6YWN0aW9ucyBjcmVhdGU6YWN0aW9ucyByZWFkOmVtYWlsX3Byb3ZpZGVyIHVwZGF0ZTplbWFpbF9wcm92aWRlciBkZWxldGU6ZW1haWxfcHJvdmlkZXIgY3JlYXRlOmVtYWlsX3Byb3ZpZGVyIGJsYWNrbGlzdDp0b2tlbnMgcmVhZDpzdGF0cyByZWFkOmluc2lnaHRzIHJlYWQ6dGVuYW50X3NldHRpbmdzIHVwZGF0ZTp0ZW5hbnRfc2V0dGluZ3MgcmVhZDpsb2dzIHJlYWQ6bG9nc191c2VycyByZWFkOnNoaWVsZHMgY3JlYXRlOnNoaWVsZHMgdXBkYXRlOnNoaWVsZHMgZGVsZXRlOnNoaWVsZHMgcmVhZDphbm9tYWx5X2Jsb2NrcyBkZWxldGU6YW5vbWFseV9ibG9ja3MgdXBkYXRlOnRyaWdnZXJzIHJlYWQ6dHJpZ2dlcnMgcmVhZDpncmFudHMgZGVsZXRlOmdyYW50cyByZWFkOmd1YXJkaWFuX2ZhY3RvcnMgdXBkYXRlOmd1YXJkaWFuX2ZhY3RvcnMgcmVhZDpndWFyZGlhbl9lbnJvbGxtZW50cyBkZWxldGU6Z3VhcmRpYW5fZW5yb2xsbWVudHMgY3JlYXRlOmd1YXJkaWFuX2Vucm9sbG1lbnRfdGlja2V0cyByZWFkOnVzZXJfaWRwX3Rva2VucyBjcmVhdGU6cGFzc3dvcmRzX2NoZWNraW5nX2pvYiBkZWxldGU6cGFzc3dvcmRzX2NoZWNraW5nX2pvYiByZWFkOmN1c3RvbV9kb21haW5zIGRlbGV0ZTpjdXN0b21fZG9tYWlucyBjcmVhdGU6Y3VzdG9tX2RvbWFpbnMgdXBkYXRlOmN1c3RvbV9kb21haW5zIHJlYWQ6ZW1haWxfdGVtcGxhdGVzIGNyZWF0ZTplbWFpbF90ZW1wbGF0ZXMgdXBkYXRlOmVtYWlsX3RlbXBsYXRlcyByZWFkOm1mYV9wb2xpY2llcyB1cGRhdGU6bWZhX3BvbGljaWVzIHJlYWQ6cm9sZXMgY3JlYXRlOnJvbGVzIGRlbGV0ZTpyb2xlcyB1cGRhdGU6cm9sZXMgcmVhZDpwcm9tcHRzIHVwZGF0ZTpwcm9tcHRzIHJlYWQ6YnJhbmRpbmcgdXBkYXRlOmJyYW5kaW5nIGRlbGV0ZTpicmFuZGluZyByZWFkOmxvZ19zdHJlYW1zIGNyZWF0ZTpsb2dfc3RyZWFtcyBkZWxldGU6bG9nX3N0cmVhbXMgdXBkYXRlOmxvZ19zdHJlYW1zIGNyZWF0ZTpzaWduaW5nX2tleXMgcmVhZDpzaWduaW5nX2tleXMgdXBkYXRlOnNpZ25pbmdfa2V5cyByZWFkOmxpbWl0cyB1cGRhdGU6bGltaXRzIGNyZWF0ZTpyb2xlX21lbWJlcnMgcmVhZDpyb2xlX21lbWJlcnMgZGVsZXRlOnJvbGVfbWVtYmVycyByZWFkOmVudGl0bGVtZW50cyByZWFkOmF0dGFja19wcm90ZWN0aW9uIHVwZGF0ZTphdHRhY2tfcHJvdGVjdGlvbiByZWFkOm9yZ2FuaXphdGlvbnMgdXBkYXRlOm9yZ2FuaXphdGlvbnMgY3JlYXRlOm9yZ2FuaXphdGlvbnMgZGVsZXRlOm9yZ2FuaXphdGlvbnMgY3JlYXRlOm9yZ2FuaXphdGlvbl9tZW1iZXJzIHJlYWQ6b3JnYW5pemF0aW9uX21lbWJlcnMgZGVsZXRlOm9yZ2FuaXphdGlvbl9tZW1iZXJzIGNyZWF0ZTpvcmdhbml6YXRpb25fY29ubmVjdGlvbnMgcmVhZDpvcmdhbml6YXRpb25fY29ubmVjdGlvbnMgdXBkYXRlOm9yZ2FuaXphdGlvbl9jb25uZWN0aW9ucyBkZWxldGU6b3JnYW5pemF0aW9uX2Nvbm5lY3Rpb25zIGNyZWF0ZTpvcmdhbml6YXRpb25fbWVtYmVyX3JvbGVzIHJlYWQ6b3JnYW5pemF0aW9uX21lbWJlcl9yb2xlcyBkZWxldGU6b3JnYW5pemF0aW9uX21lbWJlcl9yb2xlcyBjcmVhdGU6b3JnYW5pemF0aW9uX2ludml0YXRpb25zIHJlYWQ6b3JnYW5pemF0aW9uX2ludml0YXRpb25zIGRlbGV0ZTpvcmdhbml6YXRpb25faW52aXRhdGlvbnMgcmVhZDpvcmdhbml6YXRpb25zX3N1bW1hcnkgY3JlYXRlOmFjdGlvbnNfbG9nX3Nlc3Npb25zIGNyZWF0ZTphdXRoZW50aWNhdGlvbl9tZXRob2RzIHJlYWQ6YXV0aGVudGljYXRpb25fbWV0aG9kcyB1cGRhdGU6YXV0aGVudGljYXRpb25fbWV0aG9kcyBkZWxldGU6YXV0aGVudGljYXRpb25fbWV0aG9kcyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.fnjq6tJcL4hrPrhNB3mWuCO_Cwfb35Mk-W379YhAI2K0V86bY_6d3Fg-NZIyg_bk-iuUjznTUy-6Uxy1sq_fxiv5UFdvyPyQuW_r0dQ3NbTMwXJSemE8XKl8G-rQb2YbRjGnXL-RnsAGyNQdwA-4EUVPHIT95PtX7nVe39KdbcS4Yb1ZBjqkbQg0-KdMH50BeQtMisg1lc6JeVRcPKB4lxh-G5PyY3U0_8b9kYZZLmpw32nXC18n-zRHNL9LbOW45_qslVbsmpPmUEFcYKJFk2fBtsrUI4L8I9fz7I3JZKaWZrB0uxrGL6gABNLrD8a3UVkQnXGqmfbMBk_y-ROmgQ', 'content-type': 'application/json'},
+  url: `https://testtenantpasswordlessauthentication.us.auth0.com/api/v2/users/${userId}`,
+  headers: {authorization: process.env.BEARER_TOKEN, 'content-type': 'application/json'},
   data: {
     "user_metadata": {
       "companyName": id,
@@ -31,9 +44,19 @@ var options = {
 };
 
 axios.request(options).then(function (response) {
-  const auth0Response = response.data;
-  return auth0Response;
+  console.log("User update successful");
+  return response.data;
 }).catch(function (error) {
+  console.log("User update failed");
   return error;
 });
+}
+
+export const getServerSideProps = (context) => {
+  var session_token = context.query["session_token"];
+  return {
+    props: {
+      session_token,
+    },
+  };
 }
--- pages/profileUpdate.jsx
+++ pages/profileUpdate.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { useRouter } from "next/router";
 
-export default function Items(props) {
-
+export default function progressiveProf() {
+  var router = useRouter();
+  var id = router.query["session_token"];
+  const url = `${process.env.AUTH0_BASE_URL}/newtest`;
     return (
-        <form action='http://localhost:3000/newtest' method='GET'>
+        <form action={url} method='GET'>
+                <center><h1>Complete your Profile!</h1></center><br></br><br></br>
+                <center>We need you to complete your profile to personalize your experience.</center><br></br>
                 <center><label htmlFor="company_name">Company Name</label></center>
                 <center><input type="text" id="company_name" name="company_name" required /></center>
                 <br></br>
                 <center><label htmlFor="job_title">Job Title</label></center>
                 <center><input type="text" id="job_title" name="job_title" required /></center>
+                <p hidden><input type="text" name="session_token" value={id} /></p>
                 <br></br>
                 <br></br>
                 <center><button type="submit">Submit</button></center>
               </form>
             )
   }
-
-  
